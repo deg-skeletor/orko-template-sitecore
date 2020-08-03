f9570878d05294496ea34463561c53db764cedec
Ryan Heap <rheap@degdigital.com>
2020-08-03 13:54:00 -0500

    use postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const browserslist = require('browserslist');
+
 /* cssnano preset for the RTE stylesheet to prevent empty rules from being discarded and selectors with common rules to be merged */
 const rteCssNanoPreset = {
   preset: ['default', {
@@ -10,13 +12,9 @@ module.exports = ({isRte} = {}) => ({
   plugins: [
       require('postcss-easy-import'),
       require('postcss-mixins'),
-      require('postcss-custom-properties'),
-      require('postcss-custom-selectors'),
-      require('postcss-custom-media'),
-      require('postcss-color-mod-function'),
-      require('postcss-nesting'),
-      require('autoprefixer')({
-          env: 'legacy'
+      require('postcss-preset-env')({
+          stage: 1, 
+          browsers: browserslist(null, { env: 'legacy'})
       }),
       require('cssnano')(isRte ? rteCssNanoPreset : {})
   ]
