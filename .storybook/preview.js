47e926e4c3d45dc356e1982374b52c7c6713f3b0
Ryan Heap <rheap@degdigital.com>
2020-08-25 15:34:49 -0500

    upgrade to Storybook v6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,6 @@
-import { addDecorator, addParameters } from '@storybook/html';
 import reactToHtml from '@degjs/storybook-decorator-react-to-html';
 import scriptRunner from '@degjs/storybook-decorator-script-runner';
 import { withHTML } from '@whitespace/storybook-addon-html/html';
-import { withA11y } from '@storybook/addon-a11y';
 import '../source/css/global.css';
 import '../source/css/styleguide.css';
 
@@ -12,24 +10,26 @@ const dynamicModules = {
 const globalModules = {
 };
 
-/* Sort stories alphabetically */
-addParameters({
+export const parameters = {
+  /* Sort stories alphabetically */
   options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  }
-});
-
-/* Add decorators */
-addDecorator(reactToHtml);
-addDecorator(withHTML({
-  prettier: {
-    tabWidth: 4,
-    useTabs: false,
-    htmlWhitespaceSensitivity: 'ignore',
+      storySort: (a, b) =>
+          a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
-}));
-addDecorator(withA11y);
-addDecorator(scriptRunner({
-  globalModules, dynamicModules
-}));
+  a11y: {}
+};
+
+export const decorators = [
+  reactToHtml,
+  withHTML({
+      prettier: {
+          tabWidth: 4,
+          useTabs: false,
+          htmlWhitespaceSensitivity: 'ignore',
+      }
+  }),
+  scriptRunner({
+    globalModules, 
+    dynamicModules
+  })
+];
